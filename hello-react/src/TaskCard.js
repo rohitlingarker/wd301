e01481b0a7c5091fd984479712f8e1f6e4cf65f5
rohitlingarker <rohitlingerker@gmail.com>
2023-03-20 21:18:06 +0530

    first react component

--- hello-react/src/TaskCard.js
+++ hello-react/src/TaskCard.js
@@ -1,11 +1,17 @@
 import './TaskCard.css'
 
 const TaskCard = (props) => {
+  let date;
+  if (props.dueDate){
+    date="Due on:"+props.dueDate;
+  }else{
+    date="Completed on:"+props.completedAtDate;
+  }
   return (
     <div className="TaskItem">
       <h2>{props.title}</h2>
-      <p>Completed on: due date...</p>
-      <p>Assignee: name...</p>
+      <p>{date}</p>
+      <p>Assignee:{props.assigneeName}</p>
     </div>
   )
 }
