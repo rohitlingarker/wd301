75482cc9b8dda746abdcf60fbf04899bb89ecc55
rohitlingarker <rohitlingerker@gmail.com>
2023-03-20 20:56:59 +0530

    first commit

---
+++ hello-react/src/App.js
@@ -0,0 +1,20 @@
+import TaskCard from "./TaskCard";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>Pending</h1>
+        <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohit S"/>
+        <TaskCard title="Add a blog" dueDate="22nd March" assigneeName="Rohit M"/>
+      </div>
+      <div>
+        <h1>Done</h1>
+        <TaskCard title="Design the mockup" completedAtDate="10th April" assigneeName="Rohit M"/>
+        <TaskCard title="Get the approval from principal" completedAtDate="20th April" assigneeName="Ajay S"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
