cf027bca5217c5240499771c9db8311851393ca8
rohitlingarker <rohitlingerker@gmail.com>
2023-04-18 15:56:25 +0530

    wad301 l2

---
+++ hello-react/main.ts
@@ -0,0 +1,11 @@
+console.log("This is a typescript file!!!");
+type StringArray = string[];
+
+interface User {
+    name: string;
+    id: number;
+    isAdmin: boolean;
+  }
+  function printHello(): void {
+    console.log("Hello!");
+  }
