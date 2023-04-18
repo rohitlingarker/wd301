2a3bf3647a78969d2dc1e742bc986c46a14249db
rohitlingarker <rohitlingerker@gmail.com>
2023-04-18 16:17:55 +0530

    wad301 l2

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './TaskCard.css'
 
 const TaskCard = (props) => {
-  let date;
+  let date:string;
   if (props.dueDate){
-    date="Due on:"+props.dueDate;
+    date="Due on: "+props.dueDate;
   }else{
-    date="Completed on:"+props.completedAtDate;
+    date="Completed on: "+props.completedAtDate;
   }
   return (
     <div className="TaskItem">
       <h2>{props.title}</h2>
       <p>{date}</p>
-      <p>Assignee:{props.assigneeName}</p>
+      <p>Assignee: {props.assigneeName}</p>
     </div>
   )
 }
