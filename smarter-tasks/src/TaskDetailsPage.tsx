63fc9b0a3112ef0348780dddc5dc587fe7d7ebeb
rohitlingarker <rohitlingerker@gmail.com>
2023-05-01 15:21:30 +0530

    wd301 l5

--- smarter-tasks/src/TaskDetailsPage.tsx
+++ smarter-tasks/src/TaskDetailsPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useLocalStorage } from './hooks/useLocalStorage';
-import { TaskAppState, TaskItem } from "./types";
+import { TaskAppState } from "./types";
 
 interface TaskDetailsPageParams extends Record<string, string> {
   id: string;
