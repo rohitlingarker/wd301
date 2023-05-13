14d51c06113e3972d3dede2a62f7751019c341fc
rohitlingarker <rohitlingerker@gmail.com>
2023-05-13 20:28:25 +0530

    sign page updated route

--- smarter-tasks/src/pages/signup/SignupForm.tsx
+++ smarter-tasks/src/pages/signup/SignupForm.tsx
@@ -18,7 +18,7 @@ const SignupForm: React.FC = () => {
     
 
     try {
-      const response = await fetch(`${API_ENDPOINT}/organizations`, {
+      const response = await fetch(`${API_ENDPOINT}/organisations`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name: organizationName, user_name: userName, email: userEmail, password: userPassword}),
