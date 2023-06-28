7cf20191b6cd423cce95179320ddaf29092cf07b
rohitlingarker <rohitlingerker@gmail.com>
2023-06-28 10:02:42 +0530

    level 8 init

--- smarter-tasks/src/context/comment/actions.ts
+++ smarter-tasks/src/context/comment/actions.ts
@@ -4,7 +4,7 @@ export const fetchComments = async (dispatch: any,projectID: string,taskID:strin
   
   try {
     dispatch({ type: "FETCH_COMMENTS_REQUEST" });
-    const response = await fetch(`${API_ENDPOINT}/projects/${projectID}/tasks/${taskID}/comments`, {
+    const response = await fetch(`${API_ENDPOINT}/comments/${projectID}/tasks/${taskID}/comments`, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${token}` },
     });
@@ -16,35 +16,35 @@ export const fetchComments = async (dispatch: any,projectID: string,taskID:strin
   }
 };
 
-// export const addProject = async (dispatch: any, args: any) => {
-//     try {
-//       const token = localStorage.getItem("authToken") ?? "";
-//       const response = await fetch(`${API_ENDPOINT}/projects`, {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${token}` },
+export const addComment = async (dispatch: any,projectID: string,taskID:string , comment:Comment) => {
+    try {
+      const token = localStorage.getItem("authToken") ?? "";
+      const response = await fetch(`${API_ENDPOINT}/comments/${projectID}/tasks/${taskID}/comments`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', "Authorization": `Bearer ${token}` },
   
-//         // Next, I'll pass the `args` here
-//         body: JSON.stringify(args), 
-//       });
-//       if (!response.ok) {
-//         throw new Error('Failed to create project');
-//       }
-//       const data = await response.json();
-//       if (data.errors && data.errors.length > 0) {
-//         return { ok: false, error: data.errors[0].message }
-//       }
+        // Next, I'll pass the `args` here
+        body: JSON.stringify(comment), 
+      });
+      if (!response.ok) {
+        throw new Error('Failed to create comment');
+      }
+      const data = await response.json();
+      if (data.errors && data.errors.length > 0) {
+        return { ok: false, error: data.errors[0].message }
+      }
   
-//       // And if everything goes well with the API call, we will dispatch an action, 
-//       // with `type` set to `ADD_PROJECT_SUCCESS` and in `payload` we will send the 
-//       // new project `data`.
-//       dispatch({ type: 'ADD_PROJECT_SUCCESS', payload: data });
+      // And if everything goes well with the API call, we will dispatch an action, 
+      // with `type` set to `ADD_PROJECT_SUCCESS` and in `payload` we will send the 
+      // new comment `data`.
+      dispatch({ type: 'ADD_COMMENT_SUCCESS', payload: data });
   
-//       // Next, I'll return a status called "ok", with value `true`
-//       // as everything went well.
-//       return { ok: true }
-//     } catch (error) {
-//       console.error('Operation failed:', error);
-//     // Dialogue 5: And for error I'll return status called "ok", with value `false`.
-//       return { ok: false, error }
-//     }
-//   };
+      // Next, I'll return a status called "ok", with value `true`
+      // as everything went well.
+      return { ok: true }
+    } catch (error) {
+      console.error('Operation failed:', error);
+    // Dialogue 5: And for error I'll return status called "ok", with value `false`.
+      return { ok: false, error }
+    }
+  };
